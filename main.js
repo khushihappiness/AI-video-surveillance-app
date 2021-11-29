fe1fb394a50aed4d47adae2502df2f6bf1d67018
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-11-29 15:27:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,6 +15,37 @@ function preload()
 function draw()
 {
     image(video, 0, 0, 300, 300)
+
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult)
+        for(i=0; i<objects.length; i++)
+        {
+            document.getElementById("status").innerHTML="Status : Object detected";
+            document.getElementById("number_of_objects").innerHTML="Number of objects" + objects.length;
+
+    fill("#FF0000")
+    percent= floor(objects[i].confidence*100);
+    text(objects[i].label + " " + percent + "%",objects[i].x, objects[i].y)
+    noFill();
+stroke("#FF0000")
+rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+}
+
+objects=[];
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+    }
 }
 function start()
 {
